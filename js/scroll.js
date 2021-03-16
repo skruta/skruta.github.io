43d17d12420785c7eb7a847cb4f34f4bc41ea7a6
skruta <stepan.kruta13@gmail.com>
2021-03-16 14:41:28 +0100

    first deploy

---
+++ js/scroll.js
@@ -0,0 +1,23 @@
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".showcase");
+
+const sectionOneOptions = {
+    rootMargin: "-50px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver
+    (function (
+        entries,
+        sectionOneObserver
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                header.classList.add('nav-scrolled');
+            } else {
+                header.classList.remove("nav-scrolled")
+            }
+        });
+    },
+        sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
