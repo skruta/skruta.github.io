93c2de4c23194947bd2d382a8e48e9e169d0f4cb
skruta <stepan.kruta13@gmail.com>
2021-03-23 22:47:02 +0100

    finished website without portfolio works

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,11 +1,1 @@
-const menuBtn = document.querySelector('.header__nav--mobile');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
-});
+const menuBtn = document.querySelector(".nav-burger"); let menuOpen = !1; menuBtn.addEventListener("click", () => { menuOpen = menuOpen ? (menuBtn.classList.remove("open"), !1) : (menuBtn.classList.add("open"), !0) });
